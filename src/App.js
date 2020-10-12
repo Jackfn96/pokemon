6a9af8c4d6d154db0d5a7f72af67ff97df1e33ec
Jack Fraser-Nash <jackfn96@gmail.com>
2020-10-12 20:12:16 +0100

    React skeleton complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/layout/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+    <Navbar/>
     </div>
   );
 }
